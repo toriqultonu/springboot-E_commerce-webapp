90d10b1d6866774ea513cfbbbe0b5fc0dbb5d2ed
toriqultonu <toriqultonu22@gmail.com>
2024-01-07 15:39:26 +0600

    category controller updated

--- DeshDeal/src/main/java/com/example/deshdeal/controller/AdminController.java
+++ DeshDeal/src/main/java/com/example/deshdeal/controller/AdminController.java
@@ -1,23 +1,40 @@
 package com.example.deshdeal.controller;
 
+import com.example.deshdeal.model.Category;
+import com.example.deshdeal.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class AdminController {
 
+    @Autowired
+    CategoryService categoryService;
     @GetMapping("/admin")
     public String adminHome(){
         return "adminHome";
     }
 
     @GetMapping("/admin/categories")
-    public String getCategories(){
+    public String getCategories(Model model){
+        model.addAttribute("categories", categoryService.getAllCategory());
         return "categories";
     }
 
     @GetMapping("/admin/categories/add")
-    public String getCategoriesAdd(){
+    public String getCategoriesAdd(Model model){
+
+        model.addAttribute("category", new Category());
         return "categoriesAdd";
+    }
+
+    @PostMapping("/admin/categories/add")
+    public String postCategoriesAdd(@ModelAttribute("category") Category category){
+        categoryService.addCategory(category);
+        return "redirect:/admin/categories";
     }
 }
