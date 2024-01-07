2a07f8f8f5ee029dc959c05312a21cb151c73499
toriqultonu <toriqultonu22@gmail.com>
2024-01-07 12:48:01 +0600

    newly started

--- DeshDeal/src/main/java/com/koshailimited/deshdeal/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.koshailimited.deshdeal.repository;
-
-import com.koshailimited.deshdeal.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category, Integer> {
-}
